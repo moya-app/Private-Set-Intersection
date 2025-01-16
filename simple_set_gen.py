97128417baa1b1fc8f8cab5af10166545561efc0
MR-Marsden <mattrmarsden@gmail.com>
2025-01-16 11:00:49 +0200

    Testing

---
+++ simple_set_gen.py
@@ -0,0 +1,18 @@
+from random import sample
+from parameters import server_size, client_size, intersection_size
+
+#set elements can be integers < order of the generator of the elliptic curve (192 bits integers if P192 is used); 'sample' works only for a maximum of 63 bits integers.
+server_set =  sample(range(2000000000,2799999999), 1000000)
+client_set = sample(range(2000000000,2799999999), 100000)
+
+f = open('server_set', 'w')
+for item in server_set:
+	f.write(str(item) + '\n')
+f.close()
+
+g = open('client_set', 'w')
+for item in client_set:
+	g.write(str(item) + '\n')
+g.close()
+
+# Removed intersection set as not needed
