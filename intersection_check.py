6bee717ddffd1702d65cfa00bfc6869b5cf5c547
MR-Marsden <mattrmarsden@gmail.com>
2025-01-16 11:30:39 +0200

    Removal of useless files and set gen updates

--- intersection_check.py
+++
@@ -1,9 +0,0 @@
-c = open('client_set', 'r')
-client_set_entries = c.readlines()
-client_set_entries = [value.strip() for value in client_set_entries]
-
-s = open('server_set', 'r')
-server_set_entries = s.readlines()
-server_set_entries = [value.strip() for value in server_set_entries]
-
-print(sum(1 for value in client_set_entries if value in server_set_entries))
--- simple_set_gen.py
+++ simple_set_gen.py
@@ -2,8 +2,8 @@ from random import sample
 from parameters import server_size, client_size, intersection_size
 
 #set elements can be integers < order of the generator of the elliptic curve (192 bits integers if P192 is used); 'sample' works only for a maximum of 63 bits integers.
-server_set =  sample(range(2000000000,2799999999), 1000000)
-client_set = sample(range(2000000000,2799999999), 100000)
+server_set =  sample(range(2700000000,2799999999), 1000000)
+client_set = sample(range(2700000000,2799999999), 100000)
 
 f = open('server_set', 'w')
 for item in server_set:
